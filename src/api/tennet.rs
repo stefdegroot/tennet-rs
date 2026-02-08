7e090108fcea2b3e7d977136c96e03a7b38e2f7a
Stef de Groot <stefdegroot@users.noreply.github.com>
2026-02-08 18:47:25 +0100

    [TENNET-7] add OpenAPI codegen (#4)
    * basic OpenAPI documentation added
    
    * expose openAPI JSON file through the http server
    
    * remove unnecessary docker documentation
    
    * apply clippy suggestion

--- src/api/tennet.rs
+++ src/api/tennet.rs
@@ -11,5 +11,7 @@ pub fn tennet_router (app_state: AppState) -> OpenApiRouter {
         .routes(routes!(balance_delta::get_balance_delta))
         .routes(routes!(merit_order::get_merit_order))
         .routes(routes!(settlement_prices::get_settlement_prices))
+        .routes(routes!(frr_activations::get_frr_activations))
+
         .with_state(app_state)
 }
